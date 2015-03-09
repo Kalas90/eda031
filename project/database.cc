918c3f7042352f1dceff9a24275b566111a9a960
Johan Förberg <johan@forberg.se>
2015-03-09 17:41:26 +0100

    I accidentally some templates

--- project/database.cc
+++ project/database.cc
@@ -2,9 +2,13 @@
 
 using namespace std;
 
-static int read_newsgroups(void *vector_ptr, int argc, char **argv, char **col_name);
-static int read_article_list(void *vector_ptr, int argc, char **argv, char **col_name);
-static int read_article(void *article_ptr, int argc, char **argv, char **col_name);
+static int read_newsgroup_list(vector<Newsgroup>*vector_ptr, int argc, char **argv, 
+        char **col_name);
+static int read_article_list(vector<Article> *vector_ptr, int argc, char **argv, 
+        char **col_name);
+static int read_article(Article *article_ptr, int argc, char **argv, char **col_name);
+
+// Instantiate templates that we need.
 
 NewsgroupDatabase::NewsgroupDatabase(string filename)
 {
@@ -25,35 +29,17 @@ Article NewsgroupDatabase::article(unsigned newsgroup_id, unsigned article_id) c
     const string sql = 
         "select id, title, author, content from articles where newsgroup_id = " +
         newsgroup_id_str + " and id = " + article_id_str + " limit 1";
-    Article out_article("", "", "");
-    char *error_msg = nullptr;
-
-    int rc = sqlite3_exec(db, sql.c_str(), read_article, &out_article, &error_msg);
-
-    if (rc != SQLITE_OK) {
-        string  error_str(error_msg);
-        sqlite3_free(error_msg);
-        throw DatabaseError(
-                string("query fails with excuse: ") + error_str);
-    } else
-        return out_article;
+    Article out_article;
+    perform_query(sql, read_article, &out_article);
+    return out_article;
 }
 
 vector<Newsgroup> NewsgroupDatabase::list_news_groups() const
 {
     const char *sql = "select id, name from newsgroups";
     vector<Newsgroup> newsgroup_list;
-    char *error_msg = nullptr;
-
-    int rc = sqlite3_exec(db, sql, read_newsgroups, &newsgroup_list, &error_msg);
-
-    if (rc != SQLITE_OK) {
-        string error_str(error_msg);
-        sqlite3_free(error_msg);
-        throw DatabaseError(
-                string("query fails with excuse: ") + error_str);
-    } else
-        return newsgroup_list;
+    perform_query(sql, read_newsgroup_list, &newsgroup_list);
+    return newsgroup_list;
 }
 
 vector<Article> NewsgroupDatabase::list_articles(unsigned newsgroup_id)
@@ -63,67 +49,41 @@ vector<Article> NewsgroupDatabase::list_articles(unsigned newsgroup_id)
         string("select id, title, author, content from articles where newsgroup_id = ") 
         + id_str;
     vector <Article> article_list;
-    char *error_msg = nullptr;
-    int rc;
-
-    rc = sqlite3_exec(db, sql.c_str(), read_article_list, &article_list, &error_msg);
-    if (rc != SQLITE_OK) {
-        string error_str(error_msg);
-        sqlite3_free(error_msg);
-        throw DatabaseError(
-                string("query fails with excuse: ") + error_str);
-    } else 
-        return article_list;
+    perform_query(sql, read_article_list, &article_list);
+    return article_list;
 }
 
 bool NewsgroupDatabase::remove_newsgroup(unsigned id)
 {
     const string id_str = to_string(id);
-    const string delete_articles =
-        string("delete from articles where newsgroup_id = ") + id_str;
     const string delete_newsgroups = 
         string("delete from newsgroups where id = ") + id_str;
-    bool status = true;
-    char *error_msg = nullptr;
-    int rc;
+    const string delete_articles =
+        string("delete from articles where newsgroup_id = ") + id_str;
 
-    // begin transaction
-    sqlite3_exec(db, "begin", nullptr, nullptr, nullptr);
-
-    // delete the newsgroups
-    rc = sqlite3_exec(db, delete_newsgroups.c_str(), nullptr, nullptr, &error_msg);
-    if (rc != SQLITE_OK)
-        goto fail;
-
-    // did we actually delete anything? if not, give up
-    if (0 == sqlite3_changes(db)) {
-        status = false;
-        goto exit;
+    try {
+        int affected;
+        begin_transaction();
+        affected = perform_query(delete_newsgroups);
+        if (0 != affected) 
+            perform_query(delete_articles);
+        commit_transaction();
+        return 0 != affected;
+    } catch (DatabaseError ex) {
+        rollback_transaction();
+        throw;
     }
-
-    // delete owned articles
-    rc = sqlite3_exec(db, delete_articles.c_str(), nullptr, nullptr, &error_msg);
-    if (rc != SQLITE_OK)
-        goto fail;
-exit:
-    // we done. commit, deallocate and exit
-    sqlite3_exec(db, "commit", nullptr, nullptr, nullptr);
-    sqlite3_free(error_msg);
-    return status;
-
-fail:
-    // we messed up. rollback, deallocate and throw 
-    sqlite3_exec(db, "rollback", nullptr, nullptr, nullptr);
-    string error_str(error_msg);
-    sqlite3_free(error_msg);
-    throw DatabaseError(
-            string("query fails with excuse: ") + error_str);
 }
 
 bool NewsgroupDatabase::remove_article(unsigned newsgroup_id, unsigned article_id)
 {
-    (void) newsgroup_id; (void) article_id;
-    throw DatabaseError(string("remove_article: Not implemented."));
+    const string article_id_str = to_string(article_id);
+    const string newsgroup_id_str = to_string(newsgroup_id);
+    const string delete_article =
+        string("delete from articles where id = ") + article_id_str + 
+        string(" and newsgroup_id = ") + newsgroup_id_str;
+
+    return 0 != perform_query(delete_article);
 }
 
 bool NewsgroupDatabase::create_newsgroup(string name)
@@ -139,41 +99,54 @@ bool NewsgroupDatabase::create_article(unsigned newsgroup_id, string title,
     throw DatabaseError(string("create_article: Not implemented."));
 }
 
-static int read_newsgroups(void *vector_ptr, int argc, char **argv, char **col_name)
+void NewsgroupDatabase::begin_transaction()
 {
+    sqlite3_exec(db, "begin", nullptr, nullptr, nullptr);
+}
+
+void NewsgroupDatabase::commit_transaction()
+{
+    sqlite3_exec(db, "commit", nullptr, nullptr, nullptr);
+}
+
+void NewsgroupDatabase::rollback_transaction()
+{
+    sqlite3_exec(db, "rollback", nullptr, nullptr, nullptr);
+}
+
+static int read_newsgroup_list(vector<Newsgroup> *vector_ptr, int argc, char **argv, 
+        char **col_name)
+{
+    // select id, name from newsgroups
     (void) argc; (void) col_name;
-    vector<Newsgroup> *output_vector = static_cast<vector<Newsgroup>*>(vector_ptr);
     unsigned id = stoi(string(argv[0]));
     string name(argv[1]);
     Newsgroup ng(name, id);
-    output_vector->push_back(ng);
+    vector_ptr->push_back(ng);
     return 0;
 }
 
-static int read_article_list(void *vector_ptr, int argc, char **argv, char **col_name)
+static int read_article_list(vector<Article> *vector_ptr, int argc, char **argv, 
+        char **col_name)
 {
+    // select id, title, author, content from articles
     (void) argc; (void) col_name;
-    vector<Article> *output_vector = static_cast<vector<Article>*>(vector_ptr);
-    // id is 0
     string title(argv[1]);
     string author(argv[2]);
     string content(argv[3]);
-    // author, title, content
     Article article(author, title, content);
-    output_vector->push_back(article);
+    vector_ptr->push_back(article);
     return 0;
 }
 
-static int read_article(void *article_ptr, int argc, char **argv, char **col_name)
+static int read_article(Article *article_ptr, int argc, char **argv, char **col_name)
 {
+    // select id, title, content, author from articles limit 1
     (void) argc; (void) col_name;
-    Article *article = static_cast<Article*>(article_ptr);
-    // id is 0
     string title(argv[1]);
     string author(argv[2]);
     string content(argv[3]);
-    // author, title, content
-    *article = Article(author, title, content);
+    *article_ptr = Article(author, title, content);
     return 0;
 }
 
--- project/database.h
+++ project/database.h
@@ -9,7 +9,15 @@
 #include "newsgroup.h"
 #include "article.h"
 
+class DatabaseError : public std::runtime_error {
+public:
+    DatabaseError(std::string message) : std::runtime_error(message) {}
+};
+
 class NewsgroupDatabase : public NewsgroupProvider {
+private:
+    sqlite3 *db;
+
 public:
     NewsgroupDatabase(std::string filename);
     ~NewsgroupDatabase();
@@ -23,12 +31,36 @@ public:
     virtual bool create_article(unsigned int newsgroup_id,
             std::string title, std::string author, std::string text) override;
 protected:
-    sqlite3 *db;
-};
+    void begin_transaction();
+    void commit_transaction();
+    void rollback_transaction();
+    // By reading past this line, you confirm that you are 18+ and not 
+    // easily offended by ugly code.
 
-class DatabaseError : public std::runtime_error {
-public:
-    DatabaseError(std::string message) : std::runtime_error(message) {}
+    template<typename t> unsigned perform_query(const std::string &query,
+            int (*func)(t*, int, char**, char**), t *data) const 
+    {
+        char *error_msg = nullptr;
+        int (*unsafe_func)(void*, int, char**, char**) = 
+                    reinterpret_cast<int (*)(void*, int, char**, char**)>(func);
+        void *unsafe_data = static_cast<void*>(data);
+
+        int rc = sqlite3_exec(db, query.c_str(), unsafe_func, unsafe_data, &error_msg);
+
+        if (rc != SQLITE_OK) {
+            std::string error_str(error_msg);
+            sqlite3_free(error_msg);
+            throw DatabaseError(
+                    std::string("query fails with excuse: ") + error_str);
+        } else {
+            return sqlite3_changes(db);
+        }
+    }
+
+    unsigned perform_query(const std::string &query) const
+    {
+        return perform_query<void*>(query, nullptr, nullptr);
+    }
 };
 
 #endif // defined DATABASE_H 
